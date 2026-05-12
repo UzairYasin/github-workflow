0270a4127b11ada264c35973b1b1ac775dc181c4
Muhammad Uzair Bin Yasin <muzairbinyasin@gmail.com>
2026-05-12 10:27:35 +0500

    Update greeting message in app.js to include ellipsis

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.write("Hello World");
+    res.write("Hello World...");
     res.end();
   }
 });
